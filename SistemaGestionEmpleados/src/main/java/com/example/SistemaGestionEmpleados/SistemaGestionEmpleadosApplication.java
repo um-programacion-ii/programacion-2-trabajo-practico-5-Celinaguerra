32e7da0fe47f4a7e1b70cab25db33aae78933adb
celi <celinaguerradiaz@gmail.com>
2025-08-10 20:26:50 -0300

    entidades y config

---
+++ SistemaGestionEmpleados/src/main/java/com/example/SistemaGestionEmpleados/SistemaGestionEmpleadosApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaGestionEmpleados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionEmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionEmpleadosApplication.class, args);
+	}
+
+}
